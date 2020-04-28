7d3e5dc92f6733602987707765a5ecce60f76de3
Unknown <chriswm@me.com>
2020-04-28 17:03:24 +0100

    Lesson 4 - Intents and context

--- swoosh/app/src/main/java/dev/chrismercer/swoosh/MainActivity.kt
+++ swoosh/app/src/main/java/dev/chrismercer/swoosh/MainActivity.kt
@@ -1,12 +1,21 @@
 package dev.chrismercer.swoosh
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startButton.setOnClickListener { startButtonOnClick() }
+    }
+
+    private fun startButtonOnClick() {
+        val leagueIntent = Intent(this, LeagueActivity::class.java)
+        startActivity(leagueIntent)
     }
 }
